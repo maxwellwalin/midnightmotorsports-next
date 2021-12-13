3b08d0f0aa3cd88cfabfd2b35fb89e642d6aed5e
maxwell walin <maxwellwalin@gmail.com>
2021-12-13 11:30:27 -0800

    created basic page component and index page, basic rendering now works

---
+++ client/components/Page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h2>This is the page component.</h2>
+        </div>
+    )
+};
+
---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+import Page from "../components/Page";
+
+export default function IndexPage() {
+    return (
+        <div>
+            <h1>This is the home page</h1>
+            <Page />
+        </div>
+    )
+}
