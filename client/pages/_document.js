51d68a02fad46332cc8dcb45a114f39f82f47c70
maxwell walin <maxwellwalin@gmail.com>
2021-12-29 17:00:52 -0800

    transferred over public folder

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -4,32 +4,7 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang='en-US'>
-                <Head>
-                    <meta charset="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    <meta name="theme-color" content="#000000" />
-                    <link rel="icon" href="%PUBLIC_URL%/images/midnightlogo.png" />
-                    <link rel="apple-touch-icon" href="%PUBLIC_URL%/images/midnightlogo.png" />
-                    <meta name="title" property="og:title" content="Midnight Motorsports" />
-                    <meta property="og:type" content="Website" />
-                    <meta name="image" property="og:image" content="https://live.staticflickr.com/65535/51749038917_3c9b33df87_h.jpg" />
-                    <meta name="description"
-                        content="Midnight Motorsports eCommerce website where users can buy car parts for various makes, models, and years." />
-                    <meta name="author" content="Maxwell Walin, Daniel Stark, Jesus Bautista, Parth Bhatt" />
-                    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@600&display=swap" rel="stylesheet" />
-                    <link href="/css/all.css" rel="stylesheet" />
-
-                    <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
-
-                    <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin></script>
-
-                    <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
-
-                    <title>Midnight Motorsports</title>
-                </Head>
+                <Head></Head>
                 <body>
                     <Main />
                     <NextScript />
