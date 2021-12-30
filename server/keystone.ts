0b8da0fef45990e3583501f662dc3691a69e20b0
maxwell walin <maxwellwalin@gmail.com>
2021-12-30 13:30:34 -0800

    created initial keystone and types file, User schema, update gitignore

---
+++ server/keystone.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-midnight-motorsports-next';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30 * 360,
+  secret: process.env.SESSION_SECRET
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // add inital roles
+  },
+});
+
+export default withAuth(
+  // @ts-ignore
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id name email`
+    })
+  })
+)
