39a86b16c471f0b5624cdb218e0563740cf8db28
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-01 21:36:26 -0800

    Added apolloprovider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,37 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Page from "../components/Page"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Page from "../components/Page";
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <Page>
-            <Component {...pageProps} />
-        </Page>
-    )
+  return (
+    <ApolloProvider client={client}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
 }
