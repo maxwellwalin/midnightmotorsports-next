7237d3ae494ccd95da26013ecfc0dce8ec848bd1
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-03 19:41:50 -0800

    All Categories and All Models queries

--- client/lib/queries.js
+++ client/lib/queries.js
@@ -1,10 +1,19 @@
 import gql from "graphql-tag";
 
 export const ALL_CATEGORIES_QUERY = gql`
-    query ALL_CATEGORIES_QUERY {
-        allCategories {
-            id
-            name
-        }
+  query ALL_CATEGORIES_QUERY {
+    allCategories {
+      id
+      name
     }
+  }
+`;
+
+export const ALL_MODELS_QUERY = gql`
+  query ALL_MODELS_QUERY {
+    allModels {
+      id
+      name
+    }
+  }
 `;
