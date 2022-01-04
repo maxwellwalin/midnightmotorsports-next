cc92c11975f046bc818b9673545cf93c873b301d
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-03 19:41:59 -0800

    Working on creating item mutation params

--- client/lib/useForm.js
+++ client/lib/useForm.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
-  const initialValues = Object.values(initial).join('');
+  const initialValues = Object.values(initial).join("");
 
   useEffect(() => {
     // This function runs when the things we are watching change
@@ -11,11 +11,25 @@ export default function useForm(initial = {}) {
   }, [initialValues]);
 
   function handleChange(e) {
-    let { value, name, type } = e.target;
-    if (type === 'number') {
+    let value, name, type, id;
+
+    if (e?.value) {
+      value = {
+        value: e.value,
+        label: e.value,
+        type: e.type,
+        id: e.id,
+      }
+      name = e.type;
+    } else {
+      value = e.target.value;
+      name = e.target.name;
+      type = e.target.type;
+    }
+    if (type === "number") {
       value = parseInt(value);
     }
-    if (type === 'file') {
+    if (type === "file") {
       [value] = e.target.files;
     }
     setInputs({
@@ -30,7 +44,7 @@ export default function useForm(initial = {}) {
 
   function clearForm() {
     const blankState = Object.fromEntries(
-      Object.entries(inputs).map(([key, value]) => [key, ''])
+      Object.entries(inputs).map(([key, value]) => [key, ""])
     );
     setInputs(blankState);
   }
