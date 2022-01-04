57894ee27c1b2723352588c0675e92c43252b0b5
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-03 19:42:32 -0800

    Added models query and passed data to AddProduct

--- client/pages/sell.js
+++ client/pages/sell.js
@@ -1,9 +1,11 @@
 import AddProduct from "../components/AddProduct";
-import { ALL_CATEGORIES_QUERY } from "../lib/queries";
+import { ALL_CATEGORIES_QUERY, ALL_MODELS_QUERY } from "../lib/queries";
 import { useQuery } from "@apollo/client";
 
 export default function sell() {
+    const modelsResponse = useQuery(ALL_MODELS_QUERY);
     const { loading, data, error} = useQuery(ALL_CATEGORIES_QUERY);
+
 
     if (loading) {
         return <p>Loading...</p>;
@@ -11,7 +13,7 @@ export default function sell() {
     return (
         <div>
         <h1>Sell</h1>
-        <AddProduct categories={data.allCategories} />
+        <AddProduct categories={data.allCategories} models={modelsResponse.data.allModels} />
         </div>
     );
 }
