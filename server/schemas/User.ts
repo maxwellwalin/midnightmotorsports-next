d5bd5c0589e08c287511c58248303fec9e84fde4
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-05 22:28:23 -0800

    Users have proper access

--- server/schemas/User.ts
+++ server/schemas/User.ts
@@ -1,15 +1,22 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
+import { permissions, rules } from '../access';
 
 export const User = list({
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // only people with the permission can delete themselves
+    delete: permissions.canManageUsers,
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
     password: password(),
     role: relationship({
       ref: 'Role.assignedTo',
-      // TODO add access control to this
     })
-    // roles, cart, orders
+    // cart, orders
   },
 });
