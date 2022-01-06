ec2e3adbcd828ff8b1c8ce98de7d820d05c7bf9d
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-05 22:06:11 -0800

    Created Role model with crud access

---
+++ server/schemas/Role.ts
@@ -0,0 +1,24 @@
+import { relationship, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+import { permissions } from '../access';
+import { permissionFields } from './fields';
+
+export const Role = list({
+    access: {
+        create: permissions.canManageRoles,
+        read: permissions.canManageRoles,
+        update: permissions.canManageRoles,
+        delete: permissions.canManageRoles,
+    },
+    fields: {
+        name: text({ isRequired: true }),
+        ...permissionFields,
+        assignedTo: relationship({
+            ref: 'User.role', // TODO add this to user
+            many: true,
+            ui: {
+                itemView: { fieldMode: 'read' },
+            }
+        })
+    }
+})
