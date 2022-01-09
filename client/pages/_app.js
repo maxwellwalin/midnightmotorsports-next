c17d01e5b325357ca2e53eee5adaa27687b40fb6
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-08 19:37:10 -0800

    Added cart provider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,25 +1,28 @@
-import { ApolloProvider } from '@apollo/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Page from "../components/Page"
-import withData from '../lib/withData';
+import { ApolloProvider } from "@apollo/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Page from "../components/Page";
+import {CartStateProvider} from "../lib/CartState";
+import withData from "../lib/withData";
 
 function MyApp({ Component, pageProps, apollo }) {
-    return (
-        <ApolloProvider client={apollo}>
-            <Page>
-                <Component {...pageProps} />
-            </Page>
-        </ApolloProvider>
-    )
+  return (
+    <ApolloProvider client={apollo}>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
+    </ApolloProvider>
+  );
 }
 
 MyApp.getInitialProps = async function ({ Component, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    pageProps.query = ctx.query;
-    return { pageProps };
-  };
-  
-  export default withData(MyApp);
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
