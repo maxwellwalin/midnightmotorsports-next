8c2a1e471bf75492151ce1005133745b10548ac7
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-10 21:50:05 -0800

    Order and OrderItem schemas

---
+++ server/schemas/Order.ts
@@ -0,0 +1,19 @@
+import { integer, text, relationship, virtual } from "@keystone-next/fields";
+import { list } from "@keystone-next/keystone/schema";
+import { isSignedIn, permissions, rules } from "../access";
+import formatMoney from "../lib/formatMoney";
+
+export const Order = list({
+  fields: {
+    label: virtual({
+        graphQLReturnType: 'String',
+        resolver(item) {
+          return `${formatMoney(item.total)}`;
+        },
+      }),
+    total: integer(),
+    items: relationship({ ref: "OrderItem.order", many: true }),
+    user: relationship({ ref: "User.orders", }),
+    charge: text(),
+  },
+});
