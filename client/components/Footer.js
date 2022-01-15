70e5933f53cafedadc1d3b2f923a342003daf759
maxwell walin <maxwellwalin@gmail.com>
2022-01-14 23:25:54 -0800

    updated styling on layout components

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -7,45 +7,75 @@ export default function Footer() {
   return (
     <FooterStyles>
       <DisappearingDiv>
-        <DivA href="https://www.instagram.com/949midnight.motorsports/" target="_blank" rel="noreferrer">
+        <SocialA href="https://www.instagram.com/949midnight.motorsports/" target="_blank" rel="noreferrer" style={{ marginRight: '2rem' }}>
           <SocialIcon icon={faInstagram}></SocialIcon>
-        </DivA>
-        <DivA href="https://www.facebook.com/949midnight.motorsports/" target="_blank" rel="noreferrer">
+        </SocialA>
+        <SocialA href="https://www.facebook.com/949midnight.motorsports/" target="_blank" rel="noreferrer">
           <SocialIcon icon={faFacebook}></SocialIcon>
-        </DivA>
+        </SocialA>
       </DisappearingDiv>
-      <A href="https://www.google.com/maps/place/23211+Cherry+Ave,+Lake+Forest,+CA+92630/@33.6259613,-117.6915514,17z/data=!3m1!4b1!4m5!3m4!1s0x80dce90036cce33b:0x967a743faaec8edc!8m2!3d33.6259613!4d-117.6893627" target="_blank" rel='noreferrer'>23211 Cherry Ave, Lake Forest, CA 92630</A>
-      <A href="tel:9492733700">(949) 273-3700</A>
+      <StyledLink>
+        <A href="https://www.google.com/maps/place/23211+Cherry+Ave,+Lake+Forest,+CA+92630/@33.6259613,-117.6915514,17z/data=!3m1!4b1!4m5!3m4!1s0x80dce90036cce33b:0x967a743faaec8edc!8m2!3d33.6259613!4d-117.6893627" target="_blank" rel='noreferrer'>23211 Cherry Ave, Lake Forest, CA 92630</A>
+      </StyledLink>
+      <StyledLink>
+        <A href="tel:9492733700">(949) 273-3700</A>
+      </StyledLink>
     </FooterStyles>
   )
 }
 
 const FooterStyles = styled.footer`
+  background-color: #1C2541;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  height: 10rem;
-  width: 100%;
-  color: white;
-  border-top: 1px solid hsla(0,0%,100%,.1);
-list-style: none;
+  padding: 2rem;
+  border-top: 1px solid #3A506B;
+  justify-items: center;
   @media (max-width: 480px) {
     grid-template-columns: 1fr 1fr;
   }
 `
 
-const A = styled.a`
-  align-self: center;
-  justify-self: center;
+const StyledLink = styled.div`
   text-decoration: none;
   color: white;
-  font-size: 1.2rem;
-`
-
-const DivA = styled.a`
-  margin: 0 1rem;
+  transition: 400ms;
+  padding: 0.5rem 1rem;
 
   &:hover {
-    list-style: none;
-    color: gray;
+    transition: 400ms;
+    cursor: pointer;
+    color: #6FFFE9;
   }
-`
+
+  &.active {
+    color: #6FFFE9;
+  }
+`;
+
+const A = styled.a`
+  text-decoration: none;
+  color: white;
+  align-self: center;
+  transition: 400ms;
+
+  &:hover {
+    transition: 400ms;
+    cursor: pointer;
+    color: #6FFFE9;
+    border-bottom: 1px solid #6FFFE9;
+  }
+`;
+
+const SocialA = styled.a`
+  text-decoration: none;
+  color: white;
+  align-self: center;
+  transition: 400ms;
+
+  &:hover {
+    transition: 400ms;
+    cursor: pointer;
+    color: #6FFFE9;
+  }
+`;
