f08d4bfdb3dc09e3053dce0c3bb95ccc1b9902cb
maxwell walin <maxwellwalin@gmail.com>
2022-01-17 19:07:39 -0800

    added more styling to about, cleaned up file

--- client/pages/about.js
+++ client/pages/about.js
@@ -13,18 +13,14 @@ export default function About() {
           </div>
         </div>
         <img src='/images/midnightlogo-transparent.png' alt="midnight motorsports transparent background and white logo" className="brandImg" />
-
       </div>
       <div className="box2">
         <img className='lamboimg' src='https://giffiles.alphacoders.com/129/12938.gif' alt='rear end of a lambo' />
         <img className='skylineimg' src='https://giffiles.alphacoders.com/130/13051.gif' alt='a nissan r34 drifting' />
-
-
       </div>
       <div className="box3">
         <img className='supraimg' src='https://images.hdqwalls.com/download/toyota-supra-2020-tuned-4k-us-2560x1080.jpg' alt='a toyota supra' />
       </div>
-
     </Container>
   )
 }
@@ -59,6 +55,7 @@ h1 {
 .aboutPar {
   font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
   padding: 5%;
+  margin-bottom: 0;
 }
 
 .brandImg {
@@ -73,16 +70,14 @@ h1 {
 
 .lamboimg {
   max-width: 50%;
-  height: 100%;
 }
 
 .skylineimg {
-  max-width: 50% ;
-  max-height: 100%;
+  max-width: 50%;
 }
 
 .box3 {
-  margin-top: 5%;
+  margin-top: 3%;
   border-top: 40px solid #6FFFE9;
 }
 
